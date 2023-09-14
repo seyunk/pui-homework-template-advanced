9efe3fd3a4118883717b01cdb87fa7b2aa21ebd5
SeyunKim <coolseyun@gmail.com>
2023-09-14 17:48:30 -0400

    commit homework soluion 3

---
+++ solution-hw3/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Navbar from './Navbar';
+import Grid from './GridItem';
+import CartInfo from './CartBehavior'
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar></Navbar>
+        <Grid></Grid>
+        {/* 
+            Note: Here I decided to create a Grid component with Items
+            created by looping on a list of Roll objects (see GridItem.js
+            and Products.js).
+
+            The use of state and props can be found in GridItem.js.
+            
+            I think this is more convenient than creating multiple Item
+            components here and pass in props for each. In the end we will 
+            probably need to read product information from some database. 
+            
+            Multiple YouTube tutorials used a similar method.
+        */}
+    </div>
+
+    
+  );
+};
+
+export default App;
